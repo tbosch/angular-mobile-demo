2d3334033507b0b2910563bf3daa9987a1b5f2ee
Tobias Bosch <tbosch1009@gmail.com>
2013-10-24 14:20:56 -0700

    Simplified animations with route listening. Not working correctly though...

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('angularMobileDemoApp', ['ngRoute', 'ngAnimate', 'ajoslin.scrolly', 'scroll'])
+angular.module('angularMobileDemoApp', ['ngRoute', 'ngAnimate', 'ngTouch', "angular-carousel", 'ajoslin.scrolly', 'scroll'])
   .factory('$anchorScroll', function() {
     return angular.noop;
   })
@@ -8,19 +8,41 @@ angular.module('angularMobileDemoApp', ['ngRoute', 'ngAnimate', 'ajoslin.scrolly
     $routeProvider
       .when('/page1', {
         templateUrl: 'views/page1/page1.html',
-        controller: 'Page1Ctrl'
+        controller: 'Page1Ctrl',
+        depth: 0
       })
       .when('/page2', {
         templateUrl: 'views/page2/page2.html',
-        controller: 'Page2Ctrl'
+        controller: 'Page2Ctrl',
+        depth: 1
       })
       .when('/page3', {
         templateUrl: 'views/page3/page3.html',
-        controller: 'Page3Ctrl'
+        controller: 'Page3Ctrl',
+        depth: 1
       })
       .otherwise({
         redirectTo: '/page1'
       });
+  })
+  .run(function ($rootScope) {
+    $rootScope.viewSlideAnimation = 'slide-left'; //always start the page from the left
+
+    $rootScope.$on('$routeChangeStart', function(e, current, previous) {
+      $rootScope.viewSlideAnimation = solveDirection(current, previous) || $rootScope.viewSlideAnimation;
+    });
+
+    $rootScope.$on('$routeChangeSuccess', function(e, current, previous) {
+      $rootScope.viewSlideAnimation = solveDirection(current, previous, true) || $rootScope.viewSlideAnimation;
+    });
+
+    function solveDirection(current, previous, reverse) {
+      if(previous && current) {
+        return reverse ?
+          (current.depth < previous.depth ? 'slide-left' : 'slide-right') :
+          (current.depth < previous.depth ? 'slide-right' : 'slide-left');
+      }
+    }
   })
   /* css3 animations */
   .constant('navigationCssClasses', {
@@ -37,4 +59,4 @@ angular.module('angularMobileDemoApp', ['ngRoute', 'ngAnimate', 'ajoslin.scrolly
     FastClick.attach(document.body);
   });
   */
-;
+
